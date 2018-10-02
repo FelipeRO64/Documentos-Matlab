4e4e39d6532754c00e839edf7e0b5fa232148fff
FelipeRO64 <43191727+FelipeRO64@users.noreply.github.com>
2018-10-02 12:31:22 -0300

    Update convol.m

--- convol.m
+++ convol.m
@@ -3,10 +3,6 @@ function [y, ny] = convol(x, nx, h, nh)
  % Convuelve 'x' con 'h' usando 'conv' de MATLAB  
  % Genera el vector 'ny' el cual sirve como índice del vector 'y' 
  % nx = índice de x    nh = índice de h 
- x = [zeros(1,16) ones(1,10) zeros(1,30)];
-nx = -5: 50;
-[e, nh] = escalon(0, -5, 50);
-h = (0.8 .^ nh) .* e; 
  ny1 = nx(1) + nh(1);                 % primer elemento del vector ny 
  ny2 = nx(length(x)) + nh(length(h)); % último elemento del vector ny 
  ny = [ny1 : ny2];                    % crea el vector ny 
