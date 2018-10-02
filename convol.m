4e7c365462ca0e452bd346e8dbe392541a59dd46
FelipeRO64 <43191727+FelipeRO64@users.noreply.github.com>
2018-10-02 12:19:13 -0300

    Update convol.m

--- convol.m
+++ convol.m
@@ -1,9 +1,13 @@
 function [y, ny] = convol(x, nx, h, nh) 
- % Convolución modificada para incluir información de tiempo 
+ % ConvoluciÃ³n modificada para incluir informaciÃ³n de tiempo 
  % Convuelve 'x' con 'h' usando 'conv' de MATLAB  
- % Genera el vector 'ny' el cual sirve como índice del vector 'y' 
- % nx = índice de x    nh = índice de h 
+ % Genera el vector 'ny' el cual sirve como Ã­ndice del vector 'y' 
+ % nx = Ã­ndice de x    nh = Ã­ndice de h 
+ x = [zeros(1,16) ones(1,10) zeros(1,30)];
+nx = -5: 50;
+[e, nh] = escalon(0, -5, 50);
+h = (0.8 .^ nh) .* e; 
  ny1 = nx(1) + nh(1);                 % primer elemento del vector ny 
- ny2 = nx(length(x)) + nh(length(h)); % último elemento del vector ny 
+ ny2 = nx(length(x)) + nh(length(h)); % Ãºltimo elemento del vector ny 
  ny = [ny1 : ny2];                    % crea el vector ny 
  y = conv(x, h);                      % convuelve x con h --> y 
